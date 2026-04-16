f41b5b97fe68a489b7972e12112fa202419cbab0
Chris Carpita <chris.carpita@coastpay.com>
2026-04-15 23:12:46 -0400

    Paths updates

--- bash/profile.d.symlink/paths.sh
+++ bash/profile.d.symlink/paths.sh
@@ -1,27 +1,52 @@
 #!/usr/bin/env bash
 
-export PATH="/bin"
+path_reset() {
+  if [[ -z "$PATH" ]]; then
+    PATH="/usr/bin:/bin:/sbin"
+  fi
 
-paths=(
-  /sbin
-  /usr/bin
-  /usr/sbin
-  /opt/local/bin
-  /opt/local/sbin
-  /usr/local/sbin
-  /usr/local/bin
-  /snap/bin
-  /opt/homebrew/opt/rustup
-  /opt/homebrew/bin
-  $DOTFILES/bin
-  $HOME/bin
-  $HOME/.local/bin
-  $HOME/go/bin
-  ./node_modules/.bin
-  ./bin
-)
+  local os_paths=()
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    os_paths+=(/opt/homebrew/opt/rustup /opt/homebrew/bin)
+  else
+    os_paths+=(/snap/bin)
+  fi
 
-for comp in ${paths[@]}; do
-  PATH="$comp:$PATH"
-done
-alias path-reset="source ~/.profile.d/paths.sh"
+  # Entries later in the list take precedence
+  local paths=(
+    # Relative (can be unsafe, always override)
+    ./node_modules/.bin
+
+    # System
+    /bin
+    /sbin
+    /usr/bin
+    /usr/sbin
+    /opt/local/bin
+    /opt/local/sbin
+    /usr/local/sbin
+    /usr/local/bin
+
+    # OS-specific
+    ${os_paths[@]}
+
+    # User
+    ${DOTFILES:-$HOME/.dotfiles}/bin
+    $HOME/.cargo/bin
+    $HOME/.local/bin
+    $HOME/go/bin
+    $HOME/bin
+  )
+
+  local new_path=""
+  for comp in ${paths[@]}; do
+    new_path="$comp:$new_path"
+  done
+
+  PATH="$new_path"
+}
+
+path_reset
+
+alias path-reset="path_reset "
+
